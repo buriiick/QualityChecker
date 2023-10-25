74f44b3ae65d5b4dc7a7e94322d39797387c570d
buriiick <148922441+buriiick@users.noreply.github.com>
2023-10-25 14:27:26 +0300

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,24 @@
+# DEV
+connection = {
+    "host": "",
+    "port": "5433",
+    "database": "",
+    "user": "",
+    "password": ""
+}
+# TEST
+"""connection = {
+    "host": "",
+    "port": "5433",
+    "database": "",
+    "user": "",
+    "password": ""
+}
+# PROD
+connection = {
+    "host": "",
+    "port": "5433",
+    "database": "",
+    "user": "",
+    "password": ""
+}"""
